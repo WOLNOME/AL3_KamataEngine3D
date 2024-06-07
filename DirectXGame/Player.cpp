358c7c6344d6f7a87d072b238c028465f7f8c858
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-06-07 16:56:40 +0900

    Revert "2-14"
    This reverts commit 800ca77c3770c2385f2b98c615a1267389e0dbb8.

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -74,20 +74,40 @@ void Player::Update(ViewProjection& viewProjection) {
 	// 攻撃処理
 	Attack();
 
+	/// 自機のワールド座標から3Dレティクルのワールド座標を計算
+	// 自機から3Dレティクルへの距離
+	const float kDistancePlayerTo3DReticle = 50.0f;
+	// 自機から3Dレティクルへのオフセット(Z+向き)
+	Vector3 offset = {0, 0, 1.0f};
+	// 自機のワールド行列の回転を反映(offsetは自機の座標由来なので、Normalを使う←原点由来なら通常のTransform)
+	offset = TransformNormal(offset, worldTransform_.matWorld_);
+	// ベクトルの長さを整える
+	offset = Multiply(kDistancePlayerTo3DReticle, Normalize(offset));
+	// 3Dレティクルの座標を設定
+	worldTransform3DReticle_.translation_ = Add(worldTransform_.translation_, offset);
+
+	///3Dレティクルのワールド座標から2Dレティクルのスクリーン座標を計算
+	Vector3 positionReticle = worldTransform3DReticle_.translation_;
 	//ビューポート行列
 	Matrix4x4 matViewport = MakeViewportMatrix(0, 0, WinApp::kWindowWidth, WinApp::kWindowHeight, 0, 1);
 	//ビュー行列とプロジェクション行列、ビューポート行列を合成する
 	Matrix4x4 matViewProjectionViewport = Multiply(Multiply(viewProjection.matView, viewProjection.matProjection), matViewport);
-	//マウスの位置にスプライトを置く
-	ScreenToWorldMouse(matViewport, viewProjection);
+	//ワールド→スクリーン座標変換(ここで3Dから2Dになる)
+	positionReticle = Transform(positionReticle, matViewProjectionViewport);
+	//スプライトのレティクルに座標設定
+	sprite2DReticle_->SetPosition(Vector2(positionReticle.x, positionReticle.y));
 
+
+	// 3Dレティクル行列の更新
+	worldTransform3DReticle_.UpdateMatrix();
 	// 行列の更新
 	worldTransform_.UpdateMatrix();
 
 	// キャラクターの座標を画面表示する処理
 	//  デバッグテキストの表示
-	ImGui::Begin("player ");
+	ImGui::Begin(" ");
 	// float3スライダー
+
 	inputFloat3[0] = worldTransform_.translation_.x;
 	inputFloat3[1] = worldTransform_.translation_.y;
 	inputFloat3[2] = worldTransform_.translation_.z;
@@ -131,49 +151,6 @@ void Player::Attack() {
 }
 
 void Player::OnCollision() {}
-
-void Player::ScreenToWorldMouse(Matrix4x4 matViewport, ViewProjection& viewProjection) {
-	//マウス座標を取得
-	POINT mousePosition;
-	GetCursorPos(&mousePosition);
-
-	//クライアントエリア座標(スクリーン座標)に変換する
-	HWND hwnd = WinApp::GetInstance()->GetHwnd();
-	ScreenToClient(hwnd, &mousePosition);
-	//マウス座標を2Dレティクルのスプライトに代入する
-	sprite2DReticle_->SetPosition(Vector2((float)mousePosition.x, (float)mousePosition.y));
-	
-	//ビュープロジェクションビューポート合成行列
-	Matrix4x4 matVPV = Multiply(Multiply(viewProjection.matView, viewProjection.matProjection), matViewport);
-	//合成行列の逆行列を計算する
-	Matrix4x4 matInverseVPV = Inverse(matVPV);
-
-	//スクリーン座標
-	Vector3 posNear = Vector3((float)mousePosition.x, (float)mousePosition.y, 0);
-	Vector3 posFar = Vector3((float)mousePosition.x, (float)mousePosition.y, 1);
-
-	//スクリーン座標系からワールド座標系へ
-	posNear = Transform(posNear, matInverseVPV);
-	posFar = Transform(posFar, matInverseVPV);
-
-	//マウスレイの方向
-	Vector3 mouseDirection = Subtract(posFar, posNear);
-	mouseDirection = Normalize(mouseDirection);
-	//カメラから照準オブジェクトの距離
-	const float kDistanceTestObject = 50.0f;
-	worldTransform3DReticle_.translation_ = Add(posNear, Multiply(kDistanceTestObject, mouseDirection));
-	//ワールド行列の更新と転送
-	worldTransform3DReticle_.UpdateMatrix();
-
-	//デバッグ文字表示
-	ImGui::Begin("player");
-	ImGui::Text("2DReticle:(%f,%f)", sprite2DReticle_->GetPosition().x, sprite2DReticle_->GetPosition().y);
-	ImGui::Text("Near:(%+.2f,%+.2f,%+.2f)", posNear.x, posNear.y, posNear.z);
-	ImGui::Text("Far:(%+.2f,%+.2f,%+.2f)", posFar.x, posFar.y, posFar.z);
-	ImGui::Text("3DReticle:(%+.2f,%+.2f,%+.2f)", worldTransform3DReticle_.translation_.x, worldTransform3DReticle_.translation_.y, worldTransform3DReticle_.translation_.z);
-	ImGui::End();
-
-}
 
 Vector3 Player::GetWorldPosition() {
 	Vector3 worldPos;
--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,27 +1,28 @@
 #pragma once
-#include "Input.h"
 #include "Model.h"
-#include "PlayerBullet.h"
-#include "Sprite.h"
 #include "WorldTransform.h"
+#include "Sprite.h"
+#include "Input.h"
+#include "PlayerBullet.h"
 #include <list>
 
-// 前方宣言
+//前方宣言
 class GameScene;
 
 /// <summary>
 /// 自キャラ
 /// </summary>
 
-// 関数
+//関数
 
 class Player {
 public:
+
 	~Player();
 	/// <summary>
 	/// 初期化
 	/// </summary>
-	void Initialize(Model* model, uint32_t textureHandle, const Vector3& position);
+	void Initialize(Model* model, uint32_t textureHandle ,const Vector3& position);
 	/// <summary>
 	/// 更新
 	/// </summary>
@@ -35,25 +36,24 @@ public:
 	/// </summary>
 	void DrawUI();
 
-public: // 関数
+public://関数
 	/// <summary>
 	/// 攻撃
 	/// </summary>
 	void Attack();
 
-	// 衝突を検出したら呼び出されるコールバック関数
+	//衝突を検出したら呼び出されるコールバック関数
 	void OnCollision();
 
-	// マウスカーソルのスクリーン座標からワールド座標を取得して3Dレティクル配置する関数
-	void ScreenToWorldMouse(Matrix4x4 matViewport, ViewProjection& viewProjection);
+	
 
 public: // ゲッター
-	// ワールド座標を取得
+	//ワールド座標を取得
 	Vector3 GetWorldPosition();
 	// 半径
 	float GetRadius() { return rad_; };
 
-public: // セッター
+public://セッター
 	/// <summary>
 	/// 親となるワールドトランスフォームをセット
 	/// </summary>
@@ -62,23 +62,24 @@ public: // セッター
 	void SetGameScene(GameScene* gameScene) { gameScene_ = gameScene; }
 
 private:
-	// ワールド変換データ
+	//ワールド変換データ
 	WorldTransform worldTransform_;
-	// 3Dレティクル用ワールドトランスフォーム
+	//3Dレティクル用ワールドトランスフォーム
 	WorldTransform worldTransform3DReticle_;
-	// モデル
+	//モデル
 	Model* model_ = nullptr;
-	// テクスチャハンドル
+	//テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
-	// 2Dレティクル用スプライト
+	//2Dレティクル用スプライト
 	Sprite* sprite2DReticle_ = nullptr;
-	// キーボード入力
+	//キーボード入力
 	Input* input_ = nullptr;
-	// デバッグテキスト
-	float inputFloat3[3] = {0, 0, 0};
+	//デバッグテキスト
+	float inputFloat3[3] = {0,0,0};
 	// 半径
 	const float rad_ = 1.0f;
 
-	// ゲームシーン
+	//ゲームシーン
 	GameScene* gameScene_ = nullptr;
+
 };
