3ec16cafcc05d862add11da1887512479684843d
WOLNOME <143981493+WOLNOME@users.noreply.github.com>
2024-08-10 23:56:19 +0900

    a

--- DirectXGame/SceneManager.h
+++ DirectXGame/SceneManager.h
@@ -18,7 +18,7 @@ public:
 	~SceneManager();
 
 	void ChangeScene();//シーンを変える関数
-	void Update(char* keys, char* preKeys);//現在シーンの更新関数
+	void Update();//現在シーンの更新関数
 	void Draw();//現在シーンの描画関数
 private:
 
